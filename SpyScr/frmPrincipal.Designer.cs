6e9dfee16bf1f19ac68a7d6ef276a513bc8a7f2e
Roberto Amaya <betoamaya@msn.com>
2018-11-07 12:44:41 -0600

    Esquema Principal

---
+++ SpyScr/frmPrincipal.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace SpyScr
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnPausar = new System.Windows.Forms.Button();
+            this.btnMinimizar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.ttpMensajitos = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            resources.ApplyResources(this.btnIniciar, "btnIniciar");
+            this.btnIniciar.Name = "btnIniciar";
+            this.ttpMensajitos.SetToolTip(this.btnIniciar, resources.GetString("btnIniciar.ToolTip"));
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnPausar
+            // 
+            resources.ApplyResources(this.btnPausar, "btnPausar");
+            this.btnPausar.Name = "btnPausar";
+            this.ttpMensajitos.SetToolTip(this.btnPausar, resources.GetString("btnPausar.ToolTip"));
+            this.btnPausar.UseVisualStyleBackColor = true;
+            this.btnPausar.Click += new System.EventHandler(this.btnPausar_Click);
+            // 
+            // btnMinimizar
+            // 
+            resources.ApplyResources(this.btnMinimizar, "btnMinimizar");
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.ttpMensajitos.SetToolTip(this.btnMinimizar, resources.GetString("btnMinimizar.ToolTip"));
+            this.btnMinimizar.UseVisualStyleBackColor = true;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // btnSalir
+            // 
+            resources.ApplyResources(this.btnSalir, "btnSalir");
+            this.btnSalir.Name = "btnSalir";
+            this.ttpMensajitos.SetToolTip(this.btnSalir, resources.GetString("btnSalir.ToolTip"));
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmPrincipal
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnMinimizar);
+            this.Controls.Add(this.btnPausar);
+            this.Controls.Add(this.btnIniciar);
+            this.MaximizeBox = false;
+            this.Name = "frmPrincipal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.ToolTip ttpMensajitos;
+        private System.Windows.Forms.Button btnPausar;
+        private System.Windows.Forms.Button btnMinimizar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ SpyScr/frmPrincipal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpyScr
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPausar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
